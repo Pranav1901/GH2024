f0d01b1f7c92d939db3fd858febdc1fe01ff3d3e
aashish1995k <50435075+aashish1995k@users.noreply.github.com>
2024-06-27 13:07:21 +0530

    initial-rest-commit

---
+++ inshorts-rest/main.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, abort
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+shorts_post_args = reqparse.RequestParser()
+shorts_post_args.add_argument("title", type=str, help="Title of the short", required = True)
+shorts_post_args.add_argument("summary", type=str, help="Summary of the news", required = True)
+shorts_post_args.add_argument("srcLink", type=str, help="Orginal Source of the news article", required = True)
+
+
+# shorts = {
+#     1: {"title": "India won the T20 world cup", "summary": "India won the world cup with a tough fight in T20.", "srcLink": "http://newsarticle.com"},
+#     2: {"title": "Nvidia stocks reached market high", "summary": "Nvidia stocks became the most valuable stocks exceeding the Alphabet stocks", "srcLink": "http://newsarticle.com"},
+#     3: {"title": "Self-driving cars Operational in US", "summary": "Now you can go on a rider without a driver. Self-driving cars are operational in US", "srcLink": "http://newsarticle.com"},
+# }
+
+shorts = {}
+
+def abort_if_short_id_doesnt_exist(short_id):
+    if short_id not in shorts:
+        abort(404,message="Short id is not valid..")
+
+def abort_if_short_id_already_exists(short_id):
+    if short_id in shorts:
+        abort(409,message="Shorts already exist with that ID...")
+
+class Shorts(Resource):
+    def get(self, short_id):
+        abort_if_short_id_doesnt_exist(short_id)
+        return shorts[short_id]
+    
+    def post(self, short_id):
+        abort_if_short_id_already_exists(short_id)
+        args = shorts_post_args.parse_args()
+        shorts[short_id] = args
+        return shorts[short_id], 201
+    
+    def delete(self, short_id):
+        abort_if_short_id_doesnt_exist(short_id)
+        del [short_id]
+        return '', 204
+    
+ 
+api.add_resource(Shorts, "/shorts/<int:short_id>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
