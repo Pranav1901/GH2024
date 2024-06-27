8c65f007b096ce4d1dd10ea988d23b959088e72b
Thota Mounica <thotasrinagamounika@gmail.com>
2024-06-28 02:19:59 +0530

    fix the summary integration issue

--- inshorts-rest/main.py
+++ inshorts-rest/main.py
@@ -15,7 +15,7 @@ def fetch_summaries():
     response = requests.get("http://localhost:8000/articles")
     if response.status_code ==200:
         summaries = response.json()
-        result = [{'id': summary['id'], 'summary': summary['shorts'] } for summary in summaries][:5]
+        result = {summary['id']: summary['shorts'] for summary in summaries[:5]}
         return result
     else:
         return f"Error: {response.status_code}"      
@@ -26,7 +26,23 @@ def fetch_all_news():
     if response.status_code == 200:
         newsarticles = response.json()
         summaries_dict = fetch_summaries()
-        result = [{'id': article['id'], 'title': article['title'], 'content': summaries_dict, 'url': article['url'], 'urlToImage': article['urlToImage']} for article in newsarticles][:5]
+        result = []
+                # Limit to first 5 articles and enrich with summaries
+        for article in newsarticles[:5]:
+            article_id = article['id']
+                     # Check if article_id exists in summaries_dict
+            # Get summary from summaries_dict based on article ID
+            summary = summaries_dict.get(article_id, 'Summary not available')
+            enriched_article = {
+                'id': article_id,
+                'title': article['title'],
+                'content': summary,
+                'url': article['url'],
+                'urlToImage': article['urlToImage']
+            }
+            result.append(enriched_article)
+        
+
 
         return result
     else:
